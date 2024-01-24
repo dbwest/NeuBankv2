40f5c56ab71a348e23c597c452e815b85a5d8580
David West <1122691+dbwest@users.noreply.github.com>
2024-01-24 18:08:41 -0500

    fix formatting

--- bootstrap/main.tf
+++ bootstrap/main.tf
@@ -41,11 +41,11 @@ resource "azurerm_resource_group" "setup" {
 }
 
 resource "azurerm_storage_account" "sa" {
-  name                          = local.storage_account_name
-  resource_group_name           = azurerm_resource_group.setup.name
-  location                      = var.azure_location
-  account_tier                  = "Standard"
-  account_replication_type      = "LRS"
+  name                     = local.storage_account_name
+  resource_group_name      = azurerm_resource_group.setup.name
+  location                 = var.azure_location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
 }
 
 resource "azurerm_storage_container" "ct" {
