0dacd621066d6483f36a379771a51d7f266f52e0
David West <1122691+dbwest@users.noreply.github.com>
2024-01-25 14:54:53 -0500

    remove extra dot

--- modules/networking/main.tf
+++ modules/networking/main.tf
@@ -2,7 +2,7 @@ module "vnet" {
   source = "./vnet"
 
   resource_group_name = var.resource_group_name
-  resource_group_location = var.resource_group_location.
+  resource_group_location = var.resource_group_location
 
   turned_on = var.turned_on
   region = var.region
