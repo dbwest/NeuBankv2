bb2e239c3373a819ee5a415d5dad3531c341d2b1
David West <1122691+dbwest@users.noreply.github.com>
2024-01-25 14:29:11 -0500

    val -> var

--- modules/networking/vnet/main.tf
+++ modules/networking/vnet/main.tf
@@ -1,8 +1,8 @@
 resource "azurerm_virtual_network" "this" {
   count               = var.turned_on ? 1 : 0
   name                = "${var.company}-${terraform.workspace}-vnet-${var.region}"
-  location            = val.resource_group.name
-  resource_group_name = val.resource_group.location
+  location            = var.resource_group.name
+  resource_group_name = var.resource_group.location
   address_space       = ["10.0.0.0/16"]
 
   tags = lookup(module.common.tags, terraform.workspace, null)
