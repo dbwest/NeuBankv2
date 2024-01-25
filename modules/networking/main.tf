19468080bba43f74ca43dfb1127d69f8ae9114cb
David West <1122691+dbwest@users.noreply.github.com>
2024-01-25 14:47:21 -0500

    use resource group name and location in module

--- modules/networking/main.tf
+++ modules/networking/main.tf
@@ -1,8 +1,8 @@
 module "vnet" {
   source = "./vnet"
 
-  resource_group_name = azurerm_resource_group.this.name
-  resource_group_location = azurerm_resource_group.this.name
+  resource_group_name = var.resource_group_name
+  resource_group_location = var.resource_group_location.
 
   turned_on = var.turned_on
   region = var.region
